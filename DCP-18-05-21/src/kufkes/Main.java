0b3ca5729ac587a80f6ffd915daac271a1875dbc
KevinUfkes <kevinufkes@gmail.com>
2021-05-19 09:20:41 -0700

    added problem for May 19, 2021

--- DCP-18-05-21/src/kufkes/Main.java
+++ DCP-18-05-21/src/kufkes/Main.java
@@ -1,8 +1,36 @@
 package kufkes;
 
+
+
 public class Main {
 
+    public static int numSteps(int n){
+        int total;
+        int xSteps;
+        int ySteps;
+
+        xSteps = n / 2 + 1;
+
+        if(n % 2 == 0){
+            ySteps = n / 2;
+        } else {
+            ySteps = n / 2 + 1;
+        }
+
+        total = xSteps + ySteps;
+        return total;
+    }
+
+    public static int numSteps2(int n){
+
+        return 1 + ((n/2)*(n/2)) + ((n%2)*(n/2));
+    }
+
+
     public static void main(String[] args) {
-	// write your code here
+
+//        System.out.println(numSteps(3));
+        System.out.println(numSteps2(5));
+
     }
 }
