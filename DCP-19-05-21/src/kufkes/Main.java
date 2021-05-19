0d4f2e84dbf1db9091399dc118eb6a4e7a17eb2a
KevinUfkes <kevinufkes@gmail.com>
2021-05-19 15:35:29 -0700

    completed bonus problem

--- DCP-19-05-21/src/kufkes/Main.java
+++ DCP-19-05-21/src/kufkes/Main.java
@@ -5,13 +5,39 @@
 
 package kufkes;
 
+import javax.crypto.spec.PSource;
+
 public class Main {
 
-    public static boolean sum17(int [] arr) {
-
+    public static void printArr(int [] arr){
+        String s = "";
         for(int x=0; x<arr.length; x++){
-            for(int y=x+1; y<arr.length; y++){
-                if(arr[x]+arr[y]==17){
+            s += arr[x] + ", ";
+        }
+        System.out.println(s);
+    }
+
+    public static int [] selectionSort(int [] arr){
+
+       for(int x=0; x<arr.length; x++){
+           int loc = x;
+           for(int y=x+1; y<arr.length; y++){
+               if(arr[y] < arr[loc]){
+                   loc = y;
+               }
+           }
+           int temp = arr[x];
+           arr[x] = arr[loc];
+           arr[loc] = temp;
+       }
+       return arr;
+    }
+
+    public static boolean sum17Brute(int [] arr) {
+
+        for (int x = 0; x < arr.length; x++) {
+            for (int y = x + 1; y < arr.length; y++) {
+                if (arr[x] + arr[y] == 17) {
                     return true;
                 }
             }
@@ -19,13 +45,42 @@ public class Main {
         return false;
     }
 
-    public static void main(String[] args) {
-        int [] arr1  = new int [4];
-        arr1[0] = 10;
-        arr1[1] = 15;
-        arr1[2] = 3;
-        arr1[3] = 6;
+    public static boolean sum17SortWalkInward(int [] arr){
+        int front = 0;
+        int back = arr.length-1;
+        System.out.println(front + " " + back);
 
-        System.out.println(sum17(arr1));
+        while(front < back){
+            if(arr[front] + arr[back] == 17) {
+                return true;
+            } else if(arr[front] + arr[back] > 17) {
+                back--;
+            } else if (arr[front] + arr[back] < 17){
+                front++;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int [] arr1  = new int [10];
+        arr1[0] = 10;
+        arr1[1] = 13;
+        arr1[2] = 2;
+        arr1[3] = 6;
+        arr1[4] = 13;
+        arr1[5] = 8;
+        arr1[6] = 5;
+        arr1[7] = 18;
+        arr1[8] = 3;
+        arr1[9] = 16;
+
+        printArr(arr1);
+        selectionSort(arr1);
+        printArr(arr1);
+
+        System.out.println(sum17Brute(arr1));
+        System.out.println(sum17SortWalkInward(arr1));
     }
 }
