a0c8452394085eebc02115d450dcdced65e61c52
KevinUfkes <kevinufkes@gmail.com>
2021-05-19 09:41:57 -0700

    completed part 1 of problem

--- DCP-19-05-21/src/kufkes/Main.java
+++ DCP-19-05-21/src/kufkes/Main.java
@@ -2,7 +2,25 @@ package kufkes;
 
 public class Main {
 
+    public static boolean sum17(int [] arr) {
+
+        for(int x=0; x<arr.length; x++){
+            for(int y=x+1; y<arr.length; y++){
+                if(arr[x]+arr[y]==17){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int [] arr1  = new int [4];
+        arr1[0] = 10;
+        arr1[1] = 15;
+        arr1[2] = 3;
+        arr1[3] = 6;
+
+        System.out.println(sum17(arr1));
     }
 }
