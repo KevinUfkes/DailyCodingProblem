b527ed84c2b5dd2ac431ff532a9af0d7920ad868
KevinUfkes <kevinufkes@gmail.com>
2021-05-25 10:41:01 -0700

    added Node and LinkedList classes

---
+++ DCP-21-05-21/src/kufkes/LinkedList.java
@@ -0,0 +1,12 @@
+package kufkes;
+
+public class LinkedList {
+
+    private Node head;
+
+    public void add(int value){
+        Node n = new Node();
+        n
+    }
+
+}
---
+++ DCP-21-05-21/src/kufkes/Node.java
@@ -0,0 +1,11 @@
+package kufkes;
+
+public class Node {
+    private Node next;
+    private int value;
+
+    public Node(int value){
+        this.value = value;
+        this.next = null;
+    }
+}
