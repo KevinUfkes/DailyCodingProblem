ef606bf57cb953fba2ba0d6bbe05dd4daccfe947
KevinUfkes <kevinufkes@gmail.com>
2021-06-03 09:55:50 -0700

    Completed Problem for May 28, 2021

--- DCP-28-05-21-job_scheduler/src/kufkes/Main.java
+++ DCP-28-05-21-job_scheduler/src/kufkes/Main.java
@@ -1,8 +1,28 @@
+// Author: Kevin Ufkes
+// Date: June 3, 2021
+// Project: Daily Coding Problem for May 28, 2021
+// Description (MEDIUM):
+//      This problem was asked by Apple.
+//          Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
 package kufkes;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static String sampleJob(){
+        return "Job Complete!";
+    }
+
+    public static void jobScheduler(int n) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(n);
+        System.out.println(sampleJob());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start");
+        jobScheduler(2000);
+        System.out.println("End");
     }
 }
