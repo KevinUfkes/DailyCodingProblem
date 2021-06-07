be5073b3de0c2afca566b77f89c3951141187f41
KevinUfkes <kevinufkes@gmail.com>
2021-06-07 12:24:05 -0700

    created project for May 31, 2021 problem

--- DCP-30-05-21-staircase_step_combinations/src/kufkes/Main.java
+++ DCP-30-05-21-staircase_step_combinations/src/kufkes/Main.java
@@ -1,8 +1,37 @@
+// Author: Kevin Ufkes
+// Date: June 3, 2021
+// Project: Daily Coding Problem of May 30, 2021
+// Description:
+//      This problem was asked by Amazon.
+//          There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N,
+//          write a function that returns the number of unique ways you can climb the staircase.
+//          The order of the steps matters.
+//
+//          For example, if N is 4, then there are 5 unique ways:
+//
+//          1, 1, 1, 1
+//          2, 1, 1
+//          1, 2, 1
+//          1, 1, 2
+//          2, 2
+//
+//          What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+
 package kufkes;
 
 public class Main {
 
+    public static int problemSolver01(int n){
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return 1;
+        }
+        return problemSolver01(n-2) + problemSolver01(n-1);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(problemSolver01(5));
     }
 }
---
+++ DCP-31-05-21-distinct-chars-substring/src/kufkes/Main.java
@@ -0,0 +1,16 @@
+// Author: Kevin Ufkes
+// Date: June 7, 2021
+// Project: Daily Coding Problem from May 31, 2021
+// Description (HARD):
+//      This problem was asked by Amazon.
+//          Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+//          For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+package kufkes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
