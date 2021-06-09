9c7e1844200915c9ba774e14d68eb1b510e797b6
KevinUfkes <kevinufkes@gmail.com>
2021-06-08 19:00:41 -0700

    created project for June 8, 2021 problem

---
+++ DCP-07-06-21-intersecting_singly-linked-lists/src/kufkes/Main.java
@@ -0,0 +1,18 @@
+// Author: Kevin Ufkes
+// Date: June 8, 20201
+// Project: Daily Coding Problem from June 8, 2021
+// Description (EASY):
+//      This problem was asked by Google.
+//          Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
+//          For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
+//          In this example, assume nodes with the same value are the exact same node objects.
+//          Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+
+package kufkes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
--- DCP-31-05-21-distinct-chars-substring/src/kufkes/Main.java
+++ DCP-31-05-21-distinct-chars-substring/src/kufkes/Main.java
@@ -10,7 +10,75 @@ package kufkes;
 
 public class Main {
 
+    public static boolean charInString(String s, Character c){
+        for(int x=0; x<s.length(); x++){
+            if(s.charAt(x)==c){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int locLastChar(String s, Character c){
+        int loc = -1;
+        for(int x=0; x<s.length(); x++){
+            if(s.charAt(x)==c){
+                loc = x;
+            }
+        }
+        return loc;
+    }
+
+    public static int longestSubstring(String s0, int k){
+
+        int countDistinct = 0;
+        int longestS1Length = 0;
+        String s1 = "";
+
+        System.out.println("From Original String: ");
+        for(int x=0; x<s0.length(); x++){
+            System.out.print(s0.charAt(x) + ", ");
+            if(!charInString(s1, s0.charAt(x)) && countDistinct>=k){
+                int locLastChar = locLastChar(s1, s1.charAt(0));
+                countDistinct++;
+                s1 = s1.substring(locLastChar+1, s1.length());
+                s1 += s0.charAt(x);
+                if(s1.length()>longestS1Length){
+                    longestS1Length = s1.length();
+                }
+            }
+            else if(!charInString(s1, s0.charAt(x))){
+                countDistinct++;
+                s1 += s0.charAt(x);
+                if(s1.length()>longestS1Length){
+                    longestS1Length = s1.length();
+                }
+            }
+            else if(charInString(s1, s0.charAt(x))){
+                s1 += s0.charAt(x);
+                if(s1.length()>longestS1Length){
+                    longestS1Length = s1.length();
+                }
+            }
+            System.out.println(s1);
+        }
+
+        System.out.println("\n");
+        System.out.println("Solve String: ");
+        System.out.println(s1);
+        System.out.println("");
+        return longestS1Length;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Character c0 = 'a';
+        Character c1 = 'x';
+        String s0 = "abababcbcbcbxyxyxyxyxyxy";
+
+        System.out.println(longestSubstring(s0, 2));
+        System.out.println("");
+
+
+
     }
 }
