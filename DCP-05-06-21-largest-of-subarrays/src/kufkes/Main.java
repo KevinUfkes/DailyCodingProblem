c0aa04421ed3403697977cae3aaeda240e21c40a
KevinUfkes <kevinufkes@gmail.com>
2021-06-09 13:11:37 -0700

    created project for June 5, 2021 problem

---
+++ DCP-05-06-21-largest-of-subarrays/src/kufkes/Main.java
@@ -0,0 +1,21 @@
+// Author: Kevin Ufkes
+// Date: June 9, 2021
+// Project: Daily Coding Problem from June 5, 2021
+// Description (HARD):
+//      This problem was asked by Google.
+//          Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.
+//          For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+//              10 = max(10, 5, 2)
+//              7 = max(5, 2, 7)
+//              8 = max(2, 7, 8)
+//              8 = max(7, 8, 7)
+//          Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+
+package kufkes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
