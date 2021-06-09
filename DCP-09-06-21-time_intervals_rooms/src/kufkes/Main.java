4b50dae3021f2903366c0770643900374597064c
KevinUfkes <kevinufkes@gmail.com>
2021-06-09 10:37:51 -0700

    created project for June 9, 2021 problem

---
+++ DCP-09-06-21-time_intervals_rooms/src/kufkes/Main.java
@@ -0,0 +1,18 @@
+// Author: Kevin Ufkes
+// Date: June 9, 2021
+// Project: Daily Coding Problem from June 9, 2021
+// Description (EASY):
+//      This problem was asked by Snapchat.
+//          Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
+//          For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+package kufkes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+
+    }
+}
