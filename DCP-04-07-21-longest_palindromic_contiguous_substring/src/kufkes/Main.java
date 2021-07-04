5b5e2c39743acfa4f6faf915c6e17b3db8a93ba8
KevinUfkes <kevinufkes@gmail.com>
2021-07-04 10:15:25 -0700

    created project for July 4, 2021 problem

---
+++ DCP-04-07-21-longest_palindromic_contiguous_substring/src/kufkes/Main.java
@@ -0,0 +1,16 @@
+// Author: Kevin Ufkes
+// Date: July 4, 2021
+// Project: Daily Coding Problem from July 4, 2021
+// Description (HARD):
+//      This problem was asked by Amazon.
+//          Given a string, find the longest palindromic contiguous substring. If there are more than one with the maximum length, return any one.
+//          For example, the longest palindromic substring of "aabcdcb" is "bcdcb". The longest palindromic substring of "bananas" is "anana".
+
+package kufkes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
