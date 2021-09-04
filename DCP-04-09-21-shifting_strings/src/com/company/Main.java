fb926ceadf12886f8baa2fbb359528b723d9bf4d
KevinUfkes <kevinufkes@gmail.com>
2021-09-04 10:22:53 -0700

    created project for September 4, 2021 problem

---
+++ DCP-04-09-21-shifting_strings/src/com/company/Main.java
@@ -0,0 +1,16 @@
+// Author: Kevin Ufkes
+// Date: September 4, 2021
+// Project: Daily Coding Problem from September 4, 2021
+// Description (EASY):
+//      This problem was asked by Google.
+//          Given two strings A and B, return whether or not A can be shifted some number of times to get B.
+//          For example, if A is abcde and B is cdeab, return true. If A is abc and B is acb, return false.
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
--- DCP-19-07-21-x_to_the_pow_y/src/com/company/Main.java
+++ DCP-19-07-21-x_to_the_pow_y/src/com/company/Main.java
@@ -1,8 +1,28 @@
+// Author: Kevin Ufkes
+// Date: July 19, 2021
+// Project: Daily Coding Problem from July 19, 2021
+// Description (MEDIUM):
+//      This problem was asked by Google.
+//          Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+//          Do this faster than the naive method of repeated multiplication.
+//          For example, pow(2, 10) should return 1024
+
 package com.company;
 
 public class Main {
 
+    public static int pow(int x, int y){
+        int result = x;
+        for(int i=1; i<y; i++){
+            result *= x;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    int x = 2;
+	    int y = 10;
+
+        System.out.println(pow(x, y));
     }
 }
