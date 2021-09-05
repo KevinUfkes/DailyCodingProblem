f23603b95d90e98d2cbd8ad3ddc96f7e76069f20
KevinUfkes <kevinufkes@gmail.com>
2021-09-05 10:37:22 -0700

    created project for September 5, 2021 problem

---
+++ DCP-05-09-21-swapping_even-odd_bits/src/com/company/Main.java
@@ -0,0 +1,17 @@
+// Author: Kevin Ufkes
+// Date: September 5, 2021
+// Project: Daily Coding Problem from September 5, 2021
+// Description (MEDIUM):
+//      This problem was asked by Cisco.
+//          Given an unsigned 8-bit integer, swap its even and odd bits. The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.
+//          For example, 10101010 should be 01010101. 11100010 should be 11010001.
+//          Bonus: Can you do this in one line?
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
